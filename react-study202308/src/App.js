455d261e02dd003348808cf47213c7dffd278b04
GaRyung <shubom0115@naver.com>
2023-08-16 09:40:35 +0900

    동적리스트 렌더링

--- react-study202308/src/App.js
+++ react-study202308/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import ExpenseList from './components/Expenses/ExpenseList';
 import NewExpense from './components/NewExpense/NewExpense';
-import ExpenseForm from './components/NewExpense/ExpenseForm';
 
 function App() {
 
   const expenses = [
     {
+      id: 1,
       title: '바나나',
       price: 2000,
       date: new Date(2023, 3-1, 23)
     },
     {
+      id: 2,
       title: 'BBQ치킨',
       price: 20000,
       date: new Date(2023, 5-1, 21)
     },
     {
+      id: 3,
       title: '도미노피자',
       price: 35000,
       date: new Date(2023, 7-1, 14)
+    },
+    {
+      id: 4,
+      title: '엽기떡볶이',
+      price: 17000,
+      date: new Date(2021, 3-1, 28)
     },
   ];
 
