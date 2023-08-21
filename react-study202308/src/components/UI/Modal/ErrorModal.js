e77ab1a5f6fdde2252e9918cd90d5b2131f73ccb
GaRyung <shubom0115@naver.com>
2023-08-21 09:38:43 +0900

    react portal refactoring

--- react-study202308/src/components/UI/Modal/ErrorModal.js
+++ react-study202308/src/components/UI/Modal/ErrorModal.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Card from '../Card';
 import Button from '../Button/Button';
 import styles from './ErrorModal.module.css';
+import Portal from '../Portal/Portal';
 
 const BackDrop = ({onConfirm}) => {
   return (
@@ -32,22 +33,17 @@ const ModalOverlay = ({title, message, onConfirm}) => {
 const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <>
-      {
-        ReactDOM.createPortal(
-          <BackDrop onConfirm={onConfirm} />,
-          document.getElementById('backdrop-root')
-        )
-      }
-      {
-        ReactDOM.createPortal(
-          <ModalOverlay 
-            title={title}
-            message={message}
-            onConfirm={onConfirm}
-          />,
-          document.getElementById('overlay-root')
-        )
-      }
+      <Portal destId='backdrop-root'>
+        <BackDrop onConfirm={onConfirm} />
+      </Portal>
+      <Portal destId='overlay-root'>
+        <ModalOverlay 
+          title={title}
+          message={message}
+          onConfirm={onConfirm}
+        />
+      </Portal>
+     
     </>
   );
 };
