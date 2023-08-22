77c621db80cdb2f0a0911ba72b361bea65ceffd2
GaRyung <shubom0115@naver.com>
2023-08-22 10:09:20 +0900

    useEffect cleanup함수

--- react-study202308/src/components/SideEffect/Login/Login.js
+++ react-study202308/src/components/SideEffect/Login/Login.js
@@ -5,24 +5,35 @@ import styles from './Login.module.css';
 import Button from '../../UI/Button/Button';
 
 const Login = ({ onLogin }) => {
-    // 이메일 입력값을 저장
+  // 이메일 입력값을 저장
   const [enteredEmail, setEnteredEmail] = useState('');
-    // 이메일 입력이 정상적인지 확인
+  // 이메일 입력이 정상적인지 확인
   const [emailIsValid, setEmailIsValid] = useState();
-    // 패스워드 입력값을 저장
+  // 패스워드 입력값을 저장
   const [enteredPassword, setEnteredPassword] = useState('');
-    // 패스워드 입력이 정상적인 확인
+  // 패스워드 입력이 정상적인 확인
   const [passwordIsValid, setPasswordIsValid] = useState();
-    // 이메일, 패스워드가 둘다 동시에 정상적인 상태인지 확인
+  // 이메일, 패스워드가 둘다 동시에 정상적인 상태인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
 
-  // 입력란을 모드 체크하여 form의 버튼 disabled를 해제하는 상태변수
+
+  // 입력란을 모두 체크하여 form의 버튼 disabled를 해제하는 상태변수
   // formIsValid의 사이드 이펙트를 처리하는 영역
   useEffect(() => {
-    console.log('useEffect call in Login.js');
-    setFormIsValid(
-      enteredEmail.value.includes('@') && enteredPassword.trim().length > 6
-    );
+
+    setTimeout(() => {
+      console.log('useEffect call in Login.js');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 1000);
+
+
+    // cleanup 함수 - 컴포넌트가 업데이트되거나 없어지기 전에 실행
+    return () => {
+      console.log('clean up!!');
+    };
+
   }, [enteredEmail, enteredPassword]); // 이 배열에 상태변수를 넣어주면 그 상태변수가 바뀔때마다 useEffect를 재실행함
 
   const emailChangeHandler = (e) => {
@@ -78,7 +89,11 @@ const Login = ({ onLogin }) => {
           />
         </div>
         <div className={styles.actions}>
-          <Button type="submit" className={styles.btn} disabled={!formIsValid}>
+          <Button
+            type="submit"
+            className={styles.btn}
+            disabled={!formIsValid}
+          >
             Login
           </Button>
         </div>
